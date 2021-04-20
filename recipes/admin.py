13a46ca9c572a3bb2c5072a673265025b91a1429
tetyorkin <tetyorkin@gmail.com>
2021-04-21 02:03:54 +0600

    edit front

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Ingredient, IngredientItem, Recipe, Subscribe, Tag
+from .models import Ingredient, IngredientItem, Recipe, Subscribe, Tag, \
+    Favorites, ShopList
 
 
 class IngredientInline(admin.TabularInline):
@@ -33,7 +34,22 @@ class TagsAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author')
+
+
+class FavoritesAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe', 'created')
+
+
+class ShoplistAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe', 'created')
+
+
 admin.site.register(Tag, TagsAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Subscribe)
+admin.site.register(Subscribe, SubscribeAdmin)
+admin.site.register(Favorites, FavoritesAdmin)
+admin.site.register(ShopList, ShoplistAdmin)
